c52bfde87f25d44a9501063abf1cd0aef220c220
rizafauzi <rizafauzir11@gmail.com>
2020-04-22 18:26:33 +0700

    first commit

---
+++ src/main/java/com/apap/backend_tu/BackendTuApplication.java
@@ -0,0 +1,13 @@
+package com.apap.backend_tu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apap/backend_tu/BackendTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apap.backend_tu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
