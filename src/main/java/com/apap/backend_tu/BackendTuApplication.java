d8e202c8e39e72b3fb8656ee29b2fa471f7c36d6
apapGroupB <rizafra11@gmail.com>
2020-04-23 21:19:46 +0700

    backend config

--- src/main/java/com/apap/backend_tu/BackendTuApplication.java
+++ src/main/java/com/apap/backend_tu/BackendTuApplication.java
@@ -2,12 +2,30 @@ package com.apap.backend_tu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendTuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendTuApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/pengajuan-surat")
+						.allowedOrigins("http://localhost:2016")
+						.allowedMethods("GET", "POST", "OPTIONS", "PUT")
+						.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+								"Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+			}
+		};
 	}
 
 }
