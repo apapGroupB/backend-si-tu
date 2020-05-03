0d537a8b9c4d1d0c801ffeba082b58ebc3da096f
apapGroupB <rizafra11@gmail.com>
2020-05-04 05:59:36 +0700

    add path without authentication

--- src/main/java/com/apap/backend_tu/controller/RootController.java
+++ src/main/java/com/apap/backend_tu/controller/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*")
 public class RootController {
-    @RequestMapping("/")
+    @RequestMapping("/backend")
     private String home() {
         return "home";
     }
