1beabca5ab84c20aebfef53733877c283a2aeb76
Riza Fauzi Rahman <muhammad.fauzi@xapiens.id>
2020-05-24 23:07:25 +0700

    change config

--- src/main/java/com/apap/backend_tu/repository/DashboardDB.java
+++ src/main/java/com/apap/backend_tu/repository/DashboardDB.java
@@ -13,19 +13,7 @@ public interface DashboardDB extends JpaRepository<PengajuanSuratModel, Long> {
 //    @Query(value = "WITH list_date AS (SELECT ( (NOW() + INTERVAL '1 day')  + CONCAT(IFNULL((s::LONGTEXT, '') , ' day'))::VARCHAR(30) ::DATETIME(0) AS created FROM generate_series(-20, 20, 1) AS s) select new ValueModel(count(ps.id_jenis_surat), DATE_FORMAT(created, '%Y-%m-%d') as submit_date) FROM list_date left join pengajuan_surat ps on ps.tanggal_pengajuan::longtext = DATE_FORMAT(created, '%Y-%m-%d') WHERE created BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-14 AND (NOW() + INTERVAL '1 day')::DATE-EXTRACT(DOW from NOW())::INTEGER group by ps.id_jenis_surat, DATE_FORMAT(created, '%Y-%m-%d') order by submit_date;", nativeQuery = true)
 //    List<ValueModel> getPengajuanList();
 
-    @Query(value = "WITH list_date AS (\n" +
-            "    SELECT ( (NOW() + INTERVAL '1 day')  + (s::TEXT || ' day')::INTERVAL )::TIMESTAMP(0) AS created\n" +
-            "    FROM generate_series(-20, 20, 1) AS s\n" +
-            ")\n" +
-            "select count(ps.id_jenis_surat), TO_CHAR(created, 'YYYY-MM-DD') as submit_date\n" +
-            "FROM list_date \n" +
-            "left join pengajuan_surat ps\n" +
-            "on ps.tanggal_pengajuan::text = TO_CHAR(created, 'YYYY-MM-DD')\n" +
-            "WHERE created BETWEEN\n" +
-            "    NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-14\n" +
-            "    AND (NOW() + INTERVAL '1 day')::DATE-EXTRACT(DOW from NOW())::INTEGER\n" +
-            "group by ps.id_jenis_surat, TO_CHAR(created, 'YYYY-MM-DD')\n" +
-            "order by submit_date;\n", nativeQuery = true)
+    @Query(value = "select count(ps.id_jenis_surat), js.nama, ps.tanggal_pengajuan from pengajuan_surat ps join jenis_surat js on js.id = ps.id_jenis_surat GROUP by ps.id_jenis_surat, ps.tanggal_pengajuan;", nativeQuery = true)
     List<Object> getPengajuanList();
 
     @Query(value = "SELECT count(*) from pengajuan_surat", nativeQuery = true)
