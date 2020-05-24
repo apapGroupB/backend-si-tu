c82866ef500517995fec5e0feab151fd03919a8c
Riza Fauzi Rahman <muhammad.fauzi@xapiens.id>
2020-05-24 23:40:42 +0700

    change query dashboard

--- src/main/java/com/apap/backend_tu/repository/DashboardDB.java
+++ src/main/java/com/apap/backend_tu/repository/DashboardDB.java
@@ -13,7 +13,7 @@ public interface DashboardDB extends JpaRepository<PengajuanSuratModel, Long> {
 //    @Query(value = "WITH list_date AS (SELECT ( (NOW() + INTERVAL '1 day')  + CONCAT(IFNULL((s::LONGTEXT, '') , ' day'))::VARCHAR(30) ::DATETIME(0) AS created FROM generate_series(-20, 20, 1) AS s) select new ValueModel(count(ps.id_jenis_surat), DATE_FORMAT(created, '%Y-%m-%d') as submit_date) FROM list_date left join pengajuan_surat ps on ps.tanggal_pengajuan::longtext = DATE_FORMAT(created, '%Y-%m-%d') WHERE created BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-14 AND (NOW() + INTERVAL '1 day')::DATE-EXTRACT(DOW from NOW())::INTEGER group by ps.id_jenis_surat, DATE_FORMAT(created, '%Y-%m-%d') order by submit_date;", nativeQuery = true)
 //    List<ValueModel> getPengajuanList();
 
-    @Query(value = "select count(ps.id_jenis_surat) as count, ps.id_jenis_surat, ps.tanggal_pengajuan from pengajuan_surat ps where ps.tanggal_pengajuan BETWEEN DATE_SUB(NOW(), INTERVAL 14 DAY) AND NOW() GROUP by ps.id_jenis_surat, ps.tanggal_pengajuan;", nativeQuery = true)
+    @Query(value = "select count(ps.id_jenis_surat) as count, ps.id_jenis_surat, ps.tanggal_pengajuan from pengajuan_surat ps GROUP by ps.id_jenis_surat, ps.tanggal_pengajuan;", nativeQuery = true)
     List<Object> getPengajuanList();
 
     @Query(value = "SELECT count(*) from pengajuan_surat", nativeQuery = true)
